825eda7caf65408ac71b9ac49338e6613b2bea2b
susiShushi <sushishushi760@gmail.com>
2021-04-24 18:36:51 +0900

    2nd

--- python/src/sample1.py
+++ python/src/sample1.py
@@ -1,1 +1,8 @@
-from feat import Fex
+#!/usr/bin/env python3
+from feat import Detector
+face_model = "retinaface"
+landmark_model = "mobilenet"
+au_model = "rf"
+emotion_model = "resmasknet"
+detector = Detector(face_model = face_model, landmark_model=landmark_model,
+                    au_model = au_model, emotion_model=emotion_model)
